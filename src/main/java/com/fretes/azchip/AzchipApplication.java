18e6aea17f41f70bdf4c40347282d8aace1a63a2
Jesriel Rodrigues <jesrielr@hotmail.com>
2024-04-03 23:41:35 -0300

    iniciando projeto

---
+++ src/main/java/com/fretes/azchip/AzchipApplication.java
@@ -0,0 +1,13 @@
+package com.fretes.azchip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzchipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzchipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fretes/azchip/AzchipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fretes.azchip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzchipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
